e2b5459cf6dcbb28cea1eecba6ff9c34543673a1
jungminji <wjdalswl0215@gmail.com>
2022-01-10 20:09:32 +0900

    간단한 테스트 코드 작성

---
+++ src/test/java/study/core/member/MemberServiceTest.java
@@ -0,0 +1,21 @@
+package study.core.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        //given - ~~ 가 주어졌을 때
+        Member member = new Member(1L, "memberA", Grade.VIP);
+
+        //when - 그것을 했을 때
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+
+        //then - 결과
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
