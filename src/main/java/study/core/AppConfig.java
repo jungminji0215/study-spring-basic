3a3dbc4dd4d7176304909888f1c9d31944dbbc98
jungminji <wjdalswl0215@gmail.com>
2022-01-11 01:42:34 +0900

    AppConfig 리팩터링

--- src/main/java/study/core/AppConfig.java
+++ src/main/java/study/core/AppConfig.java
@@ -1,5 +1,6 @@
 package study.core;
 
+import study.core.discount.DiscountPolicy;
 import study.core.discount.FixDiscountPolicy;
 import study.core.member.MemberService;
 import study.core.member.MemberServiceImpl;
@@ -9,14 +10,23 @@ import study.core.order.OrderServiceImpl;
 
 // 생성자 주입을 해보자
 // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+// AppConfig는 중복이 없고 역할에 따른 구현이 잘 보여야 한다.
 public class AppConfig {
 
     public MemberService memberService() {
         // 참조 값을 넘겨주는 것.
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
